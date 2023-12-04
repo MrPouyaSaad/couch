e22c72499a4532f30c5b169fdbb6d96869a3befd
Pouya Sadeghzadeh <122441650+MrPouyaSaad@users.noreply.github.com>
2023-12-04 23:12:10 +0330

    Add train model

---
+++ lib/model/training.dart
@@ -0,0 +1,11 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+class Train {
+  String name;
+  String reps;
+  String imagePath;
+  Train({
+    required this.name,
+    required this.reps,
+    required this.imagePath,
+  });
+}
