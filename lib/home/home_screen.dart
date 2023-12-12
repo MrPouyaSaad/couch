ffe31f38987cca79e9d4dc3d59e0740d1476f0cd
Pouya Sadeghzadeh <122441650+MrPouyaSaad@users.noreply.github.com>
2023-12-12 19:04:05 +0330

    add print package

--- lib/home/home_screen.dart
+++ lib/home/home_screen.dart
@@ -4,26 +4,96 @@ import 'dart:io';
 
 import 'package:animated_custom_dropdown/custom_dropdown.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import 'package:open_file/open_file.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:pdf/pdf.dart';
 import 'package:pdf/widgets.dart' as pw;
 
 import 'package:couch/model/exercise.dart';
 import 'package:couch/model/list.dart';
+import 'package:couch/model/training.dart';
+
+List<Train> train = [];
 
 class HomeScreen extends StatelessWidget {
   HomeScreen({super.key});
   final pw.Document pdf = pw.Document();
 
-  Future<String> savePdf() async {
+  Future<String> savePdf(List exerxice) async {
     final pw.Document pdf = pw.Document();
-
+    var iranYekanFont = pw.Font.ttf(
+      await rootBundle.load("assets/fonts/iranyekanwebregular.ttf"),
+    );
     pdf.addPage(
       pw.Page(
+        textDirection: pw.TextDirection.rtl,
+        theme: pw.ThemeData.withFont(
+          base: iranYekanFont,
+        ),
+        pageFormat: PdfPageFormat.a4,
         build: (pw.Context context) {
-          return pw.Center(
-            child: pw.Text('Hello World!'),
+          return pw.ListView.builder(
+            itemCount: exerxice.length + 1,
+            itemBuilder: (context, index) {
+              if (index == 0) {
+                return pw.Row(
+                  mainAxisAlignment: pw.MainAxisAlignment.spaceBetween,
+                  children: [
+                    pw.Text(
+                      'برنامه تمرینی',
+                      // style: pw.TextStyle(
+                      //     fontWeight: pw.FontWeight.bold, fontSize: 18),
+                    ),
+                    pw.Text(
+                      'استاد پویا صاد',
+                      // style: TextStyle(
+                      //     fontFamily: 'Iran-Nastaliq',
+                      //     fontSize: 32,
+                      //     fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                );
+              } else {
+                return pw.Column(
+                  children: [
+                    pw.Container(
+                      width: double.infinity,
+                      child: pw.Row(
+                        children: [
+                          pw.Container(
+                            margin: pw.EdgeInsets.only(right: 4),
+                            decoration: pw.BoxDecoration(
+                              shape: pw.BoxShape.circle,
+                            ),
+                            child: pw.Text(
+                              '$index',
+                            ),
+                          ),
+                          pw.Divider(),
+                        ],
+                      ),
+                    ),
+                    pw.Container(
+                      width: double.infinity,
+                      margin: pw.EdgeInsets.symmetric(vertical: 16),
+                      child: pw.Column(
+                        children: [
+                          pw.Row(
+                            children: [
+                              pw.Text(exerxice[index - 1].name),
+                              pw.Text(exerxice[index - 1].reps),
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                    if (index == exerxice.length) pw.Divider(),
+                  ],
+                );
+              }
+            },
           );
         },
       ),
@@ -38,6 +108,15 @@ class HomeScreen extends StatelessWidget {
     return path;
   }
 
+  Future<void> saveFile(document, String name) async {
+    final Directory? dir = await getDownloadsDirectory();
+    final File file = File('${dir?.path}/$name.pdf');
+
+    await file.writeAsBytes(await document.save());
+    debugPrint('Saved exported PDF at: ${file.path}');
+    OpenFile.open(file.path);
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -45,7 +124,7 @@ class HomeScreen extends StatelessWidget {
         floatingActionButton: FloatingActionButton.extended(
           onPressed: () async {
             try {
-              final String path = await savePdf();
+              final String path = await savePdf(train);
               ScaffoldMessenger.of(context).showSnackBar(
                 SnackBar(content: Text('PDF saved at $path')),
               );
@@ -147,7 +226,6 @@ class AddExerciseItem extends StatefulWidget {
     required this.exercises,
   }) : super(key: key);
   final List<ExerciseModel> exercises;
-
   @override
   State<AddExerciseItem> createState() => _AddExerciseItemState();
 }
@@ -156,6 +234,20 @@ class _AddExerciseItemState extends State<AddExerciseItem> {
   final basePath = 'assets/images/';
   bool isSelected = false;
   String imagePath = '';
+  late TextEditingController _controller;
+  @override
+  void initState() {
+    super.initState();
+    _controller = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+
+    _controller.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Row(
@@ -183,12 +275,17 @@ class _AddExerciseItemState extends State<AddExerciseItem> {
               setState(() {
                 imagePath = value.imagePath;
                 isSelected = true;
+                train.add(Train(
+                  name: value.name,
+                  reps: _controller.text,
+                  imagePath: imagePath,
+                ));
               });
               log('changing value to: $value');
             },
           ),
         ),
-        CountOfExercise(),
+        CountOfExercise(controller: _controller),
         if (isSelected)
           Expanded(
             child: Image.asset(basePath + imagePath),
@@ -223,9 +320,10 @@ class ExerciseNumber extends StatelessWidget {
 
 class CountOfExercise extends StatefulWidget {
   const CountOfExercise({
-    super.key,
-  });
-
+    Key? key,
+    required this.controller,
+  }) : super(key: key);
+  final TextEditingController controller;
   @override
   State<CountOfExercise> createState() => _CountOfExerciseState();
 }
@@ -233,19 +331,6 @@ class CountOfExercise extends StatefulWidget {
 class _CountOfExerciseState extends State<CountOfExercise> {
   String count = '';
   bool isEmpty = false;
-  late TextEditingController _controller;
-  @override
-  void initState() {
-    super.initState();
-    _controller = TextEditingController();
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-
-    _controller.dispose();
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -270,7 +355,7 @@ class _CountOfExerciseState extends State<CountOfExercise> {
           content: Directionality(
             textDirection: TextDirection.rtl,
             child: TextField(
-              controller: _controller,
+              controller: widget.controller,
               textAlign: TextAlign.center,
               keyboardType: TextInputType.phone,
               textDirection: TextDirection.ltr,
@@ -283,9 +368,9 @@ class _CountOfExerciseState extends State<CountOfExercise> {
           actions: [
             TextButton(
               onPressed: () {
-                if (_controller.text.isNotEmpty) {
+                if (widget.controller.text.isNotEmpty) {
                   setState(() {
-                    count = _controller.text;
+                    count = widget.controller.text;
                     isEmpty = false;
                   });
                   Navigator.of(context).pop();
@@ -328,10 +413,10 @@ class Header extends StatelessWidget {
         ),
         Text(
           'استاد پویا صاد',
-          style: TextStyle(
-              fontFamily: 'Iran-Nastaliq',
-              fontSize: 32,
-              fontWeight: FontWeight.bold),
+          // style: TextStyle(
+          //     fontFamily: 'Iran-Nastaliq',
+          //     fontSize: 32,
+          //     fontWeight: FontWeight.bold),
         ),
       ],
     );
